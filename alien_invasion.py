277c56ab799914c63e01387ac7b35c720c16dd88
Georg Allikas <greg_182@msn.com>
2025-07-31 11:39:50 +0300

    Add ending the game: alien-ship collisions & aliens reach bottom

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,10 @@
 import sys
+from time import sleep
 
 import pygame
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -27,19 +29,27 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
 
+        # Create an instance to store game statistics.
+        self.stats = GameStats(self)
+
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
 
+        # Start Alien Invasion in an active state.
+        self.game_active = True
+
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            if self.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
             self.clock.tick(60)
 
@@ -89,7 +99,7 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
 
         self._check_bullet_alien_collisions()
-        
+
     def _check_bullet_alien_collisions(self):
         """Check for any bullets that have hit aliens"""
         # If so, get rid of the bullet and the alien
@@ -101,13 +111,45 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
 
+    def _ship_hit(self):
+        """Respond to the ship being hit bu an alien"""
+        if self.stats.ships_left > 0:
+            # Decrement ships left.
+            self.stats.ships_left -= 1
 
+            # Get rid of any remaining aliens and bullets
+            self.bullets.empty()
+            self.aliens.empty()
+
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
+
+            # Pause.
+            sleep(0.5)
+        else:
+            self.game_active = False
 
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet"""
         """Check if the fleet is at an edge, then update positions."""
         self._check_fleet_edges()
         self.aliens.update()
+
+        # Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            self._ship_hit()
+
+        # Look for aliens hitting the bottom of the screen.
+        self._check_aliens_bottom()
+
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen"""
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= self.settings.screen_height:
+                # Treat this the same as if the ship got hit.
+                self._ship_hit()
+                break
 
     def _create_fleet(self):
         """Create the fleet of aliens"""
--- ship.py
+++ ship.py
@@ -1,12 +1,13 @@
 import pygame
 
+
 class Ship:
     """A class to manage ship."""
 
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position"""
         self.screen = ai_game.screen
-        self.settings = ai_game.settings 
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
@@ -23,6 +24,11 @@ class Ship:
         self.moving_right = False
         self.moving_left = False
 
+    def center_ship(self):
+        """Center the ship on the screen"""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+
     def update(self):
         """Update the ship's postion based on the movement flag."""
         # Update the ship's x value, not the rect.
@@ -32,7 +38,7 @@ class Ship:
             self.x -= self.settings.ship_speed
 
         # Update rect object from self.x
-        self.rect.x = self.x 
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
